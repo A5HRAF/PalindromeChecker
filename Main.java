61b175e2dd96d1f77a9909f31701fb810995e65c
ASHRAF <95166565+A5HRAF@users.noreply.github.com>
2022-02-04 04:06:54 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Taking input");
+        Scanner iscan = new Scanner(System.in);
+        System.out.println("Enter Text(Lowercase only) : ");
+        String a = iscan.next();
+        String b = a;
+        StringBuilder sb = new StringBuilder(b).reverse();
+        b = sb.toString();
+
+
+        if (a.equals(b)) {
+            System.out.println("yes, given word is a palindrome");
+        } else {
+            System.out.println("no, given word is not a palindrome");
+        }
+
+    }
+}
